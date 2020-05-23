8d5d28d1153cf20ca6708b4abaff599b9afa5a1b
James Pierson <jameswpbusiness@gmail.com>
2020-05-23 00:28:06 -0400

    Added input, rows, logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import SearchFuntion from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log("This is my initializer")
+
+
+    const movies = [
+      {id: 0, title: "Avengers: Infinity War", overview: "filler"},
+      {id: 1, title: "Avengers: Infinity War", overview: "filler"},
+    ]
+
+    this.state = {rows: [ 
+      <p key="1">This is my rows</p>
+    ]}
+
+    var movieRows = []
+    movies.forEach((movie) => {
+      console.log(movie.title)
+      movieRows.push(<p>Movie title: {movie.title}</p>)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+                <img width="100" alt="logo" src="logo.png"/>
+              </td>
+              <td width="8"/>
+              <td>
+                 <h3>MoviesDB Search</h3>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input style={{
+          fontSize: 24,
+          display: 'block',
+          width: '99%',
+          paddingTop: 8,
+          paddingBottom: 8,
+          paddingLeft: 16
+        }} placeholder="Enter search tearm"/>
+
+        {this.state.rows}
+      </div>
+    );
+  }
 }
 
 export default App;
